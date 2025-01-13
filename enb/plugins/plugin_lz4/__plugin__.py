28f579c6429592a97ec3459d9b8c56c0bdea2c4e
xfernandez <1531272@uab.cat>
2025-01-13 12:12:04 +0100

    modify plugin lz4 and makefile

--- enb/plugins/plugin_lz4/__plugin__.py
+++ enb/plugins/plugin_lz4/__plugin__.py
@@ -8,6 +8,6 @@ class LZ4Plugin(enb.plugins.PluginMake):
     contrib_authors = ["Yann Collet"]
     contrib_reference_urls = ["https://lz4.github.io/lz4/"]
     contrib_download_url_name = [
-        ("https://github.com/miguelinux314/experiment-notebook/blob/dev/contrib/lz4-1.9.2.zip?raw=true",
-         "lz4-1.9.2.zip")]
+        ("https://github.com/PauQuintasTorra/experiment-notebook/blob/dev/contrib/lz4-1.10.0.zip?raw=true",
+         "lz4-1.10.0.zip")]
     tested_on = {"linux"}
