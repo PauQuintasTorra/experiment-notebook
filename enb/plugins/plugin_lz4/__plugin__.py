2ca5c8b154aa7c44af1df77faeabe07b9ea8d3d4
xfernandez <1531272@uab.cat>
2025-01-13 12:18:14 +0100

    update url name lz4

--- enb/plugins/plugin_lz4/__plugin__.py
+++ enb/plugins/plugin_lz4/__plugin__.py
@@ -8,6 +8,6 @@ class LZ4Plugin(enb.plugins.PluginMake):
     contrib_authors = ["Yann Collet"]
     contrib_reference_urls = ["https://lz4.github.io/lz4/"]
     contrib_download_url_name = [
-        ("https://github.com/PauQuintasTorra/experiment-notebook/blob/dev/contrib/lz4-1.10.0.zip?raw=true",
+        ("https://github.com/PauQuintasTorra/experiment-notebook/blob/master/contrib/lz4-1.10.0.zip?raw=true",
          "lz4-1.10.0.zip")]
     tested_on = {"linux"}
